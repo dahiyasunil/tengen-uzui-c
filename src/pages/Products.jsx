3aad71a67b5ca1e9575678627d4733f433097f60
dahiyasunil <sunil.dahiya13@gmail.com>
2025-01-20 18:56:21 +0530

    feat: added filter aside ui component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchProducts } from "../features/productSlice";
 import ProductCard from "../components/ProductCard";
+import FilterAside from "../components/FilterAside";
 
 const Products = () => {
   const dispatch = useDispatch();
@@ -13,10 +14,17 @@ const Products = () => {
 
   return (
     <div className="container py-10">
-      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {products.map((product) => (
-          <ProductCard product={product} key={product._id} />
-        ))}
+      <div className="grid md:grid-cols-12">
+        <aside className="col-span-3 hidden text-sm md:mr-8 md:block md:py-4">
+          <FilterAside />
+        </aside>
+        <section className="col-span-9">
+          <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3">
+            {products.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
+        </section>
       </div>
     </div>
   );
