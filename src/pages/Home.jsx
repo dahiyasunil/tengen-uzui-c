1c611646f5a7b5ff52373ac24c018729c9be2f28
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-05 06:42:21 +0530

    chore: fetch products when home page is loaded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import NewCollectionSection from "../components/NewCollectionSection";
 import PromoSection from "../components/PromoSection";
+import { fetchProducts } from "../features/productSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
   return (
     <div>
       <PromoSection />
