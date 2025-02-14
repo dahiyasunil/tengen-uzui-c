0aeaf6caefa9c48a89721dac3f2cf639e37ae702
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-14 21:59:09 +0530

    feat: added thunk and extrareducer for adding item to cart when user is logged in

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -3,6 +3,23 @@ import axios from "axios";
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
+const user = {
+  mobileNumber: null,
+  name: "",
+  emailId: "",
+  addresses: [],
+  wishlist: [],
+  bag: [],
+  orders: [],
+};
+
+const initialState = {
+  user,
+  loggedIn: false,
+  status: "idle",
+  error: null,
+};
+
 export const loginUser = createAsyncThunk(
   "user/login",
   async (mobileNumber) => {
@@ -49,22 +66,30 @@ export const removeFromWishlist = createAsyncThunk(
   },
 );
 
-const user = {
-  mobileNumber: null,
-  name: "",
-  emailId: "",
-  addresses: [],
-  wishlist: [],
-  bag: [],
-  orders: [],
-};
+export const addItemToCartThunk = createAsyncThunk(
+  "user/addItemToCart",
+  async (itemData, { getState }) => {
+    const { productObjId, quantity } = itemData;
+    const userId = getState().user.user._id || getState().user.user.user._id;
+    const response = await axios.put(`${serverUrl}/api/user/cart/add`, {
+      userId,
+      productObjId,
+      quantity,
+    });
+    return response.data;
+  },
+);
 
-const initialState = {
-  user,
-  loggedIn: false,
-  status: "idle",
-  error: null,
-};
+export const getCartItems = createAsyncThunk(
+  "user/getCartDetails",
+  async (_, { getState }) => {
+    const userId = getState().user.user._id || getState().user.user.user._id;
+    const response = await axios.get(
+      `${serverUrl}/api/user/cart/details/${userId}`,
+    );
+    return response.data;
+  },
+);
 
 const userSlice = createSlice({
   name: "user",
@@ -126,6 +151,29 @@ const userSlice = createSlice({
     });
     builder.addCase(getWishlistItems.rejected, (state, action) => {
       state.status = "failedToFetchWishlistItems";
+      state.error = action.payload;
+    });
+    builder.addCase(addItemToCartThunk.pending, (state) => {
+      state.status = "addingItemToCart";
+    });
+    builder.addCase(addItemToCartThunk.fulfilled, (state, action) => {
+      state.status = "addedItemToCart";
+      state.user.bag = action.payload.cart;
+    });
+    builder.addCase(addItemToCartThunk.rejected, (state, action) => {
+      state.status = "failedToAddToCart";
+      state.error = action.payload;
+    });
+
+    builder.addCase(getCartItems.pending, (state) => {
+      state.status = "fetchingCartItems";
+    });
+    builder.addCase(getCartItems.fulfilled, (state, action) => {
+      state.status = "cartItemsRetrived";
+      state.user.bag = action.payload.cart;
+    });
+    builder.addCase(getCartItems.rejected, (state, action) => {
+      state.status = "failedToFetchCartItems";
       state.error = action.payload;
     });
   },
