7e600eb9675b0b1240f5c866a0b51220ef681b13
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-14 21:58:02 +0530

    feat: added dispatch call for adding item to cart when user is logged in

--- src/utils/cartHandler.js
+++ src/utils/cartHandler.js
@@ -1,7 +1,12 @@
-import { addItemToCartAction, removeFromWishlist } from "../features/userSlice";
+import {
+  addItemToCartAction,
+  removeFromWishlist,
+  addItemToCartThunk,
+} from "../features/userSlice";
 
 export const addToCart = async (dispatch, loggedIn, product) => {
   if (loggedIn) {
+    dispatch(addItemToCartThunk({ productObjId: product._id, quantity: 1 }));
   } else {
     dispatch(addItemToCartAction(product));
   }
