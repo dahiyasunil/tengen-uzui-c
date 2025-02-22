a0a5698bb6a0cf6ad6416feb0dabbfee5675fc1e
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-22 10:25:27 +0530

    chore: added Toaster to layout and routes for user details and edit user details component , also removed strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,24 @@ import Wishlist from "./components/user/Wishlist";
 import Cart from "./components/user/Cart";
 import Profile from "./components/user/Profile";
 import UserDetails from "./components/user/UserDetails";
+import EditUserDetails from "./components/user/EditUserDetails";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <Router>
       <Header />
+      <div>
+        <Toaster position="top-right" />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/userDetails" element={<UserDetails />} />
+        <Route path="/user/details" element={<UserDetails />} />
+        <Route path="/user/edit" element={<EditUserDetails />} />
         <Route path="/productDetails/:productId" element={<ProductDetails />} />
       </Routes>
     </Router>
