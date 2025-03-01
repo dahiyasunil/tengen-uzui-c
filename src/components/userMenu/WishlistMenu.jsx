47cd670742e3cf1179cb3e4d84635c84e467879f
dahiyasunil <sunil.dahiya13@gmail.com>
2025-03-01 22:57:16 +0530

    feat: added indicator to show number or wishlist item on wishlist icon

--- src/components/userMenu/WishlistMenu.jsx
+++ src/components/userMenu/WishlistMenu.jsx
@@ -4,7 +4,10 @@ import { HeartIcon } from "@heroicons/react/24/outline";
 
 const WishlistMenu = ({ loginDialogHandler }) => {
   const navigate = useNavigate();
-  const { loggedIn } = useSelector((state) => state.user);
+  const {
+    loggedIn,
+    user: { wishlist },
+  } = useSelector((state) => state.user);
 
   const wishlistHandler = () => {
     if (loggedIn) {
@@ -16,7 +19,16 @@ const WishlistMenu = ({ loginDialogHandler }) => {
 
   return (
     <div className="flex flex-col">
-      <button onClick={wishlistHandler}>
+      <button onClick={wishlistHandler} className="relative">
+        {wishlist && wishlist.length > 0 && (
+          <span className="absolute bottom-3 left-6 flex size-5">
+            <span className="absolute inline-flex h-full w-full rounded-full bg-red-600 bg-opacity-90">
+              <small className="absolute bottom-0.5 left-1 text-xs text-white">
+                {wishlist.length}
+              </small>
+            </span>
+          </span>
+        )}
         <HeartIcon className="size-5 justify-self-center text-grey-300 transition duration-200 hover:text-grey-700" />
       </button>
       <span className="text-xs">Wishlist</span>
